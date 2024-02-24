244191a45a740833c11b25bf2137d2926ebc1d36
Vishue1892 <victorsanchesbarrietos1203@gmail.com>
2024-02-23 19:08:23 -0600

    05 React server components -use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola mundo pagina de inicio');
   return (
     <main className={styles.main}>
     <h1>Hola mundo</h1>
