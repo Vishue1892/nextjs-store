522112916864f407ce86ef2e460daeadaadae672
Vishue1892 <victorsanchesbarrietos1203@gmail.com>
2024-04-18 18:23:52 -0600

    36 Manejo de cookies para token de acceso en proyecto

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,41 @@
 "use server"
+import { GraphQLClientSingleton } from "app/graphql"
+import { createCartMutation } from "app/graphql/mutations/createCartMutation"
+import { createUserMutation } from "app/graphql/mutations/createUserMutation"
+import { createAccessToken } from "app/utils/auth/createAccessToken"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
-export const handleCreateUser = (formData) =>{
-    console.log("handleCreateUser", formData);
+export const handleCreateUser = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData)
+  delete formDataObject["password_confirmation"]
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const variables = {
+    input: {
+      ...formDataObject,
+      phone: '+52' + formDataObject.phone
+    }
+  }
+
+  const { customerCreate }: {
+    customerCreate: {
+      customer: {
+        firstName: string
+        email: string
+      }
+    }
+  } = await graphqlClient.request(createUserMutation, variables)
+  const { customer } = customerCreate
+  if (customer?.firstName) {
+    await createAccessToken(formDataObject.email as string, formDataObject.password as string)
+    redirect('/store')
+  }
+}
+
+export const handleLogin = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData)
+  const accesToken = await createAccessToken(formDataObject.email as string, formDataObject.password as string)
+  if (accesToken) {
+    redirect('/store')
+  }
 }
